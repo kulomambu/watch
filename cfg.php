e630540cedd9abe3a7f0da4fd2cddf9fcae0ef5d
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-31 14:31:32 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,13 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 9; ASUS_X00TD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+
+$autologin = "ses_user%3Dkulomambu888%26ses_hash%3Dc7d181ec33373e084ead4acfc6e37331";
+
+$ssid = "vll8kpalvv2mi8bjg6j1vb8cm1";
+
+
+
+//  DATA LOGIN
+$username = "kulomambu888";
+$pass = "zxcvcxz123";
